0b9eeb1df54e4e021dddf8e4075b9523416ae6d2
JPychon <64781464+JPychon@users.noreply.github.com>
2022-09-26 16:51:40 -0700

    Server-side project files

---
+++ Server/Models/Player/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Server.Models
+{
+    public class User 
+    {
+        public Character Character { get; set; }
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public DateTime Created { get; set; }
+
+
+
+        public User()
+        {
+            this.Id = Guid.NewGuid();
+            this.Created = DateTime.UtcNow;
+        }
+
+    }
+}
